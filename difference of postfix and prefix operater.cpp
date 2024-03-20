9013178653164147ace6406a16e25264f1b9e417
dev_hanif <152361547+hanifabbas786@users.noreply.github.com>
2024-03-20 22:45:01 +0500

    Initial commit

---
+++ difference of postfix and prefix operater.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a ,b,x,y;
+	a=b=x=y=0;
+	a++;
+	b=a;
+	++x;
+	y=x;
+	cout<<"a:"<<a<<endl;
+	cout<<"b:"<<b<<endl;
+	cout<<"x:"<<x<<endl;
+	cout<<"y:"<<y<<endl;
+
+	return 0;
+}
+
